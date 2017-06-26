a1950350b6d209b3cc1398a95716ea3065bb3cc1
linceln <LIN2016ce>
2017-06-26 10:41:54 +0800

    add indicator padding

--- library/src/main/java/com/xyz/library/TabLayout.java
+++ library/src/main/java/com/xyz/library/TabLayout.java
@@ -33,7 +33,7 @@ public class TabLayout extends LinearLayout {
     private Paint indicatorPaint;
     private int itemWidth;
     private float indicatorHeight = getDp(4);
-    private int indicatorPadding = getDp(24);
+    private float indicatorPadding = getDp(16);
 
     // line
     private Paint linePaint;
@@ -73,6 +73,9 @@ public class TabLayout extends LinearLayout {
         }
         if (a.hasValue(R.styleable.TabLayout_lineColor)) {
             lineColor = a.getColor(R.styleable.TabLayout_lineColor, lineColor);
+        }
+        if (a.hasValue(R.styleable.TabLayout_indicatorPadding)) {
+            indicatorPadding = a.getDimension(R.styleable.TabLayout_indicatorPadding, indicatorPadding);
         }
         a.recycle();
 
