7da8516c382a9f643b879fc62cd4f1aa4f4101bc
linceln <LIN2016ce>
2017-06-27 09:27:54 +0800

    20170627

--- library/src/main/java/com/xyz/library/TabLayout.java
+++ library/src/main/java/com/xyz/library/TabLayout.java
@@ -61,6 +61,7 @@ public class TabLayout extends LinearLayout {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public TabLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        setOrientation(LinearLayout.HORIZONTAL);
         init(context, attrs);
     }
 
@@ -159,21 +160,22 @@ public class TabLayout extends LinearLayout {
             }
 
             @Override
-            public void onPageScrolled(int position, float offset, int px) {
+            public void onPageScrolled(final int position, float offset, int px) {
+
                 if (offset >= 0.0f && offset <= 0.5f) {
-                    if(!isPastHalf) {
+                    if (!isPastHalf) {
                         isPastHalf = true;
                         setSelected(position);
                     }
-                    scrollOffset = getActualWidth() / itemCount * offset * 2;
+                    scrollOffset = itemWidth * offset * 2;
                     getPath(position);
                     invalidate();
                 } else if (offset > 0.5f && offset < 1.0f) {
-                    if(isPastHalf) {
+                    if (isPastHalf) {
                         isPastHalf = false;
                         setSelected(position + 1);
                     }
-                    scrollOffset = getActualWidth() / itemCount - getActualWidth() / itemCount * (1 - offset) * 2;
+                    scrollOffset = itemWidth - itemWidth * (1 - offset) * 2;
                     path = new Path();
                     path.moveTo(position * itemWidth + scrollOffset + indicatorPadding, getHeight());
                     path.lineTo((position + 2) * itemWidth - indicatorPadding, getHeight());
